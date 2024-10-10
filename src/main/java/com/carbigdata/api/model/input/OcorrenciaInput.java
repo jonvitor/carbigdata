39d247d8c353a1174f45ed32999e7bf2fbbb8092
Joao <2uftv86j0ehkxkcz8tzfxo9krt73pu@bots.bitbucket.org>
2024-10-10 20:51:20 -0300

    Adicionando mensagens em  OcorrenciaInput

--- src/main/java/com/carbigdata/api/model/input/OcorrenciaInput.java
+++ src/main/java/com/carbigdata/api/model/input/OcorrenciaInput.java
@@ -3,7 +3,6 @@ package com.carbigdata.api.model.input;
 import java.time.OffsetDateTime;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
